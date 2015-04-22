ac0ca01d8ba7bef710029de374f7aaa1cd29cb9a
Anthony Kuske <anthonykuske@gmail.com>
2015-04-22 23:20:42 +0200

    Version bump

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015042104; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version = 2015042200; // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires = 2012112900; // Requires this Moodle version
 $plugin->component = 'block_homework'; // Full name of the plugin (used for diagnostics)
